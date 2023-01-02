3384c46c6c514c20ebc6e4ed8a5ab5868aa9e846
MrEnergetico <114943713+MrEnergetico@users.noreply.github.com>
2023-01-02 23:36:25 +0100

    alpha

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 function cambiaColore() {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      var textarea = document.getElementById("textarea");
 
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    var textarea = document.getElementById("textarea");
-
-    if (data.blacklist.includes(textarea.value)) {
-      textarea.style.backgroundColor = "red";
-    } else {
-      textarea.style.backgroundColor = "green";
-    }
-    
-  });
-
+      if (data.blacklist.includes(textarea.value)) {
+        textarea.style.backgroundColor = "red";
+      } else {
+        textarea.style.backgroundColor = "green";
+      }
+    });
 }
 
-// Chiamata della funzione cambiaColore ogni volta che l'evento input viene triggerato sulla text area
 textarea.addEventListener("input", cambiaColore);
-
-// Chiamata della funzione cambiaColore quando il documento viene caricato
 window.addEventListener("load", cambiaColore);
+
+function isLink(text) {
+  const linkRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  return linkRegex.test(text);
+}
+
+function Link() {
+  var textareas = document.getElementById("textareas");
+  if (isLink(textareas.value)) {
+    console.log(textareas.value);
+  }
+}
+
+textarea.addEventListener("input", Link);
